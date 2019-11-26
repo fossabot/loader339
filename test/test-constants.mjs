c38c3f6938e92e577e826e93318609afd8425f17
Derek Lewis <DerekNonGeneric@inf.is>
2019-11-26 10:48:24 -0500

    🧪 add version check to test

--- test/test-constants.mjs
+++ test/test-constants.mjs
@@ -1,11 +1,10 @@
 import tap from 'tap';
-import { resolve } from 'path';
-
-import { projectRoot } from 'lorem/constants'
+import { projectRoot } from 'lorem/constants';
 
 const root = '/'
 
 tap.test(t => {
+  t.ok(process.versions.node.match('13.2.0'), 'Node version must be 13.2.0.');
   t.ok(projectRoot.includes('lorem-demo'), 'path should include lorem-demo');
   t.ok(!projectRoot.includes('lib'), 'it should not include lib');
   t.done();
