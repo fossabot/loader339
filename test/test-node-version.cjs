7e8cd7f8f62ebb62a7e4495451e417a40a732435
Derek Lewis <DerekNonGeneric@inf.is>
2020-02-03 08:47:30 -0500

    🧪 get test working

--- test/test-node-version.cjs
+++ test/test-node-version.cjs
@@ -1,11 +1,14 @@
-const tap = require('tap');
-const semver = require('semver');
 const pjson = require('../package.json');
+const semver = require('semver');
+const tap = require('tap');
 
- // Accommodate for labels and build metadata appearing as SemVer extensions.
- const NODE_VERSION = process.version.split('-')[0];
+// Accommodate for labels and build metadata appearing as SemVer extensions.
+const NODE_VERSION = process.version.split('-')[0];
 
-tap.test(t => {
-    t.ok(semver.satisfies(NODE_VERSION, pjson.engines.node), 'Node version must be correct.');
-    t.done();
+tap.test((t) => {
+  t.ok(
+    semver.satisfies(NODE_VERSION, pjson.engines.node),
+    'Node version must be correct.'
+  );
+  t.done();
 });
