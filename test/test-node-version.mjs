faa09aa0984a20c9837609a05c4ff2ebf1109624
Derek Lewis <DerekNonGeneric@inf.is>
2020-02-06 02:30:06 -0500

    🔔 modernize pjson & test format

--- test/test-node-version.mjs
+++ test/test-node-version.mjs
@@ -1,3 +1,6 @@
+import { createRequire } from 'module';
+
+const require = createRequire(import.meta.url);
 const pjson = require('../package.json');
 const semver = require('semver');
 const tap = require('tap');
@@ -8,7 +11,7 @@ const NODE_VERSION = process.version.split('-')[0];
 tap.test((t) => {
   t.ok(
     semver.satisfies(NODE_VERSION, pjson.engines.node),
-    `current node version, ${NODE_VERSION}, should satisfy ${pjson.engines.node}`
+    `current \`node\`, ${NODE_VERSION}, should satisfy ${pjson.engines.node}`
   );
   t.done();
 });
