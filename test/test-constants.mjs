fbd375962508f6ee140f0a51b357893b9cb2d21d
Derek Lewis <DerekNonGeneric@inf.is>
2020-02-12 08:41:41 -0500

    💨 bring constants test up to speed

--- test/test-constants.mjs
+++ test/test-constants.mjs
@@ -1,11 +1,8 @@
+import { projectRoot } from 'loader339/constants';
 import tap from 'tap';
-import { projectRoot } from 'lorem/constants';
 
-const root = '/'
-
-tap.test(t => {
-  t.ok(process.versions.node.match('13.2.0'), 'Node version must be 13.2.0.');
-  t.ok(projectRoot.includes('lorem-demo'), 'path should include lorem-demo');
+tap.test((t) => {
+  t.ok(projectRoot.includes('loader339'), 'path should include loader339');
   t.ok(!projectRoot.includes('lib'), 'it should not include lib');
   t.done();
 });
